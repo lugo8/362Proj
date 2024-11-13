348324eb1b9edb3ae604e0ad21dba52eda72c901
Austin Lugo <austinslugo@gmail.com>
2024-11-13 16:06:21 -0500

    Finished saving to buffer as midi data

--- src/main.c
+++ src/main.c
@@ -3,6 +3,10 @@
 #include <math.h>   // for M_PI
 #include <stdio.h>
 #include <stdlib.h>
+#include "ff.h"   // FatFS library
+#include "diskio.h" // SD card driver
+#include <string.h>
+
 
 void internal_clock();
 
@@ -20,13 +24,13 @@ void internal_clock();
 // float bNotes[9] = {30.868000, 61.736000, 123.472000, 246.944000, 493.888000, 987.776001, 1975.552002, 3951.104004, 7902.208008};
 
 float noteMatrix[7][9] = {
-                        {27.500000, 55.000000, 110.000000, 220.000000, 440.000000, 880.000000, 1760.000000, 3520.000000, 7040.000000}, //A note
-                        {30.868000, 61.736000, 123.472000, 246.944000, 493.888000, 987.776001, 1975.552002, 3951.104004, 7902.208008}, //B note
-                        {16.351999, 32.703999, 65.407997, 130.815994, 261.631989, 523.263977, 1046.527954, 2093.055908, 4186.111816},  //C note
-                        {18.354000, 36.708000, 73.416000, 146.832001, 293.664001, 587.328003, 1174.656006, 2349.312012, 4698.624023},  //D note
-                        {20.601999, 41.203999, 82.407997, 164.815994, 329.631989, 659.263977, 1318.527954, 2637.055908, 5274.111816},  //E note
-                        {21.827000, 43.653999, 87.307999, 174.615997, 349.231995, 698.463989, 1396.927979, 2793.855957, 5587.711914},  //F note
-                        {24.500000, 49.000000, 98.000000, 196.000000, 392.000000, 784.000000, 1568.000000, 3136.000000, 6272.000000}}; //G note
+                        {27.500000, 55.000000, 110.000000, 220.000000, 440.000000, 880.000000, 1760.000000, 3520.000000}, //A note
+                        {30.868000, 61.736000, 123.472000, 246.944000, 493.888000, 987.776001, 1975.552002, 3951.104004}, //B note
+                        {16.351999, 32.703999, 65.407997, 130.815994, 261.631989, 523.263977, 1046.527954, 2093.055908},  //C note
+                        {18.354000, 36.708000, 73.416000, 146.832001, 293.664001, 587.328003, 1174.656006, 2349.312012},  //D note
+                        {20.601999, 41.203999, 82.407997, 164.815994, 329.631989, 659.263977, 1318.527954, 2637.055908},  //E note
+                        {21.827000, 43.653999, 87.307999, 174.615997, 349.231995, 698.463989, 1396.927979, 2793.855957},  //F note
+                        {24.500000, 49.000000, 98.000000, 196.000000, 392.000000, 784.000000, 1568.000000, 3136.000000}}; //G note
 
 
 
@@ -87,6 +91,8 @@ int VOLUME = 64;
 int OCTAVE = 4;
 
 noteDuration* headOfNotes = NULL;
+uint8_t buffer[1000] = {0};  // Buffer to store the MIDI data
+
 
 
 
@@ -104,8 +110,7 @@ void freeList(noteDuration* head) {
 noteDuration* createNode(char note, float duration) {
     noteDuration* newNode = (noteDuration*)malloc(sizeof(noteDuration));
     if (newNode == NULL) {
-        printf("Memory allocation failed\n");
-        exit(1);
+        return NULL;
     }
     newNode->note = note;
     newNode->duration = duration;
@@ -131,51 +136,91 @@ void pushBack(noteDuration* head, char note, float duration)
 
 }
 
-void write_variable_length(FILE *file, uint32_t value) {
-    uint8_t buffer[4];
-    int i = 0;
 
+
+void write_variable_length(uint8_t *buffer, uint32_t value, int *index) {
     do {
-        buffer[i] = value & 0x7F; // Keep the lower 7 bits
-        if (i > 0) {
-            buffer[i] |= 0x80; // Set the MSB for continuation
+        buffer[*index] = value & 0x7F;  // Keep the lower 7 bits
+        if (*index > 0) {
+            buffer[*index] |= 0x80;  // Set the MSB for continuation
         }
-        value >>= 7; // Shift right by 7 bits
-        i++;
+        value >>= 7;  // Shift right by 7 bits
+        (*index)++;
     } while (value > 0);
-
-    // Write the bytes in reverse order (most significant byte first)
-    for (int j = i - 1; j >= 0; j--) {
-        fputc(buffer[j], file);
-    }
 }
 
-void write_tempo_meta_event(FILE *file, uint32_t tempo) {
+void write_tempo_meta_event(uint8_t *buffer, uint32_t tempo, int *index) {
     // Tempo meta-event format: 0xFF 0x51 0x03 t1 t2 t3
-    fputc(0x00, file);    // Delta time (0 for immediate effect)
-    fputc(0xFF, file);    // Meta-event
-    fputc(0x51, file);    // Tempo event type
-    fputc(0x03, file);    // Length of the data (3 bytes)
+    buffer[*index] = 0x00; (*index)++;  // Delta time
+    buffer[*index] = 0xFF; (*index)++;  // Meta-event
+    buffer[*index] = 0x51; (*index)++;  // Tempo event type
+    buffer[*index] = 0x03; (*index)++;  // Length of the data (3 bytes)
 
     // Split the 24-bit tempo value into three bytes (big-endian)
-    fputc((tempo >> 16) & 0xFF, file);  // Byte 1
-    fputc((tempo >> 8) & 0xFF, file);   // Byte 2
-    fputc(tempo & 0xFF, file);          // Byte 3
+    buffer[*index] = (tempo >> 16) & 0xFF; (*index)++;  // Byte 1
+    buffer[*index] = (tempo >> 8) & 0xFF;  (*index)++;  // Byte 2
+    buffer[*index] = tempo & 0xFF;         (*index)++;  // Byte 3
 }
 
-void write_note_on(FILE *file, uint8_t channel, uint8_t note, uint8_t velocity) {
-    fputc(0x00, file);                  // Delta time (0 for immediate effect)
-    fputc(0x90 | (channel & 0x0F), file); // Note-on event for the given channel
-    fputc(note, file);                  // Note number (e.g., 60 for middle C)
-    fputc(velocity, file);              // Velocity (e.g., 64 for medium)
+void write_note_on(uint8_t *buffer, uint8_t channel, uint8_t note, uint8_t velocity, int *index) {
+    buffer[*index] = 0x00; (*index)++;  // Delta time
+    buffer[*index] = 0x90 | (channel & 0x0F); (*index)++;  // Note-on event for the given channel
+    buffer[*index] = note; (*index)++;  // Note number (e.g., 60 for middle C)
+    buffer[*index] = velocity; (*index)++;  // Velocity (e.g., 64 for medium)
 }
 
-void write_note_off(FILE *file, uint32_t delta_time, uint8_t channel, uint8_t note, uint8_t velocity) {
-    write_variable_length(file, delta_time); // Delta time before note-off
-    fputc(0x80 | (channel & 0x0F), file);    // Note-off event for the given channel
-    fputc(note, file);                       // Note number
-    fputc(velocity, file);                   // Velocity (usually 0)
+void write_note_off(uint8_t *buffer, uint32_t delta_time, uint8_t channel, uint8_t note, uint8_t velocity, int *index) {
+    write_variable_length(buffer, delta_time, index);  // Delta time before note-off
+    buffer[*index] = 0x80 | (channel & 0x0F); (*index)++;  // Note-off event for the given channel
+    buffer[*index] = note; (*index)++;  // Note number
+    buffer[*index] = velocity; (*index)++;  // Velocity (usually 0)
 }
+
+// void write_variable_length(FILE *file, uint32_t value) {
+//     uint8_t buffer[4];
+//     int i = 0;
+
+//     do {
+//         buffer[i] = value & 0x7F; // Keep the lower 7 bits
+//         if (i > 0) {
+//             buffer[i] |= 0x80; // Set the MSB for continuation
+//         }
+//         value >>= 7; // Shift right by 7 bits
+//         i++;
+//     } while (value > 0);
+
+//     // Write the bytes in reverse order (most significant byte first)
+//     for (int j = i - 1; j >= 0; j--) {
+//         fputc(buffer[j], file);
+//     }
+// }
+
+// void write_tempo_meta_event(FILE *file, uint32_t tempo) {
+//     // Tempo meta-event format: 0xFF 0x51 0x03 t1 t2 t3
+//     fputc(0x00, file);    // Delta time (0 for immediate effect)
+//     fputc(0xFF, file);    // Meta-event
+//     fputc(0x51, file);    // Tempo event type
+//     fputc(0x03, file);    // Length of the data (3 bytes)
+
+//     // Split the 24-bit tempo value into three bytes (big-endian)
+//     fputc((tempo >> 16) & 0xFF, file);  // Byte 1
+//     fputc((tempo >> 8) & 0xFF, file);   // Byte 2
+//     fputc(tempo & 0xFF, file);          // Byte 3
+// }
+
+// void write_note_on(FILE *file, uint8_t channel, uint8_t note, uint8_t velocity) {
+//     fputc(0x00, file);                  // Delta time (0 for immediate effect)
+//     fputc(0x90 | (channel & 0x0F), file); // Note-on event for the given channel
+//     fputc(note, file);                  // Note number (e.g., 60 for middle C)
+//     fputc(velocity, file);              // Velocity (e.g., 64 for medium)
+// }
+
+// void write_note_off(FILE *file, uint32_t delta_time, uint8_t channel, uint8_t note, uint8_t velocity) {
+//     write_variable_length(file, delta_time); // Delta time before note-off
+//     fputc(0x80 | (channel & 0x0F), file);    // Note-off event for the given channel
+//     fputc(note, file);                       // Note number
+//     fputc(velocity, file);                   // Velocity (usually 0)
+// }
 
 long calcTime(float sec)
 {
@@ -225,81 +270,154 @@ int getNoteNum(char note)
 }
 
 
-
-void writeNotes(noteDuration* head, FILE* file)
-{
+void writeNotes(noteDuration* head, uint8_t *buffer) {
     noteDuration* iter = head;
+    int index = 0;
 
-    if(head == NULL)
-    {
+    if (head == NULL) {
         return;
     }
 
-    while(iter != NULL)
-    {     
-        //Duration
+    while (iter != NULL) {
+        // Duration
         long duration = calcTime(iter->duration);
-
         int noteNum = getNoteNum(iter->note);
 
-        //TODO: implement volume 
-
         // Write note-on event
-        write_note_on(file, 0, noteNum, VOLUME);
+        write_note_on(buffer, 0, noteNum, VOLUME, &index);
 
-        //Write note off event
-        write_note_off(file, duration, 0, noteNum, 0);
+        // Write note-off event
+        write_note_off(buffer, duration, 0, noteNum, 0, &index);
 
         iter = iter->next;
     }
 }
 
-void write2File(noteDuration* notes)
-{
-    FILE *file = fopen("output.mid", "wb");
 
-    if (!file) {
-        perror("Failed to open file");
-        return;
-    }
+void write2Array(noteDuration* notes) {
+    int index = 0;
 
     // Write MIDI header chunk
-    fwrite("MThd", 1, 4, file);
-    fputc(0x00, file); fputc(0x00, file); fputc(0x00, file); fputc(0x06, file); // Header length (6 bytes)
-    fputc(0x00, file); fputc(0x01, file); // Format type (1 for multi-track)
-    fputc(0x00, file); fputc(0x01, file); // Number of tracks (1)
-    fputc(0x01, file); fputc(0xE0, file); // Division (480 ticks per quarter note)
+    memcpy(&buffer[index], "MThd", 4); index += 4;
+    buffer[index++] = 0x00;
+    buffer[index++] = 0x00;
+    buffer[index++] = 0x00;
+    buffer[index++] = 0x06;  // Header length (6 bytes)
+    buffer[index++] = 0x00;
+    buffer[index++] = 0x01;  // Format type (1 for multi-track)
+    buffer[index++] = 0x00;
+    buffer[index++] = 0x01;  // Number of tracks (1)
+    buffer[index++] = 0x01;
+    buffer[index++] = 0xE0;  // Division (480 ticks per quarter note)
 
     // Write track chunk header
-    fwrite("MTrk", 1, 4, file);
-    fputc(0x00, file); fputc(0x00, file); fputc(0x00, file); fputc(0x00, file); // Placeholder for track length
+    memcpy(&buffer[index], "MTrk", 4); index += 4;
+    buffer[index++] = 0x00;
+    buffer[index++] = 0x00;
+    buffer[index++] = 0x00;
+    buffer[index++] = 0x00;  // Placeholder for track length
 
     // Write tempo meta-event (e.g., 500,000 μs/qn for 120 BPM)
-    write_tempo_meta_event(file, 500000);
+    write_tempo_meta_event(buffer, 500000, &index);
 
-    //Write all notes to file
-    writeNotes(notes, file);
+    // Write all notes to array
+    writeNotes(notes, buffer);
 
     // Write end-of-track meta-event
-    fputc(0x00, file);    // Delta time (immediately after last event)
-    fputc(0xFF, file);    // Meta-event
-    fputc(0x2F, file);    // End-of-track event type
-    fputc(0x00, file);    // Length (0 bytes)
+    buffer[index++] = 0x00;  // Delta time (immediately after last event)
+    buffer[index++] = 0xFF;  // Meta-event
+    buffer[index++] = 0x2F;  // End-of-track event type
+    buffer[index++] = 0x00;  // Length (0 bytes)
 
     // Go back and write the actual track length (subtract 8 bytes for "MTrk" and length placeholder)
-    long end_position = ftell(file);
-    long track_length = end_position - 22; // Header (14 bytes) + "MTrk" (4 bytes) + length field (4 bytes)
-    fseek(file, 18, SEEK_SET); // Move to the length field position
-    fputc((track_length >> 24) & 0xFF, file);
-    fputc((track_length >> 16) & 0xFF, file);
-    fputc((track_length >> 8) & 0xFF, file);
-    fputc(track_length & 0xFF, file);
-    fseek(file, end_position, SEEK_SET); // Move back to the end of the file
+    long track_length = index - 22;  // Header (14 bytes) + "MTrk" (4 bytes) + length field (4 bytes)
 
-    freeList(notes);
-    fclose(file);
-    return;
+    // Update the track length in the array
+    buffer[18] = (track_length >> 24) & 0xFF;
+    buffer[19] = (track_length >> 16) & 0xFF;
+    buffer[20] = (track_length >> 8) & 0xFF;
+    buffer[21] = track_length & 0xFF;
+
+    // Now the buffer contains the MIDI data
+
+    // You can now do something with the buffer, such as sending it over UART, or saving it to a different storage
 }
+
+
+// void writeNotes(noteDuration* head, FILE* file)
+// {
+//     noteDuration* iter = head;
+
+//     if(head == NULL)
+//     {
+//         return;
+//     }
+
+//     while(iter != NULL)
+//     {     
+//         //Duration
+//         long duration = calcTime(iter->duration);
+
+//         int noteNum = getNoteNum(iter->note);
+
+//         //TODO: implement volume 
+
+//         // Write note-on event
+//         write_note_on(file, 0, noteNum, VOLUME);
+
+//         //Write note off event
+//         write_note_off(file, duration, 0, noteNum, 0);
+
+//         iter = iter->next;
+//     }
+// }
+
+// void write2File(noteDuration* notes)
+// {
+//     FILE *file = fopen("output.mid", "wb");
+
+//     if (!file) {
+//         perror("Failed to open file");
+//         return;
+//     }
+
+//     // Write MIDI header chunk
+//     fwrite("MThd", 1, 4, file);
+//     fputc(0x00, file); fputc(0x00, file); fputc(0x00, file); fputc(0x06, file); // Header length (6 bytes)
+//     fputc(0x00, file); fputc(0x01, file); // Format type (1 for multi-track)
+//     fputc(0x00, file); fputc(0x01, file); // Number of tracks (1)
+//     fputc(0x01, file); fputc(0xE0, file); // Division (480 ticks per quarter note)
+
+//     // Write track chunk header
+//     fwrite("MTrk", 1, 4, file);
+//     fputc(0x00, file); fputc(0x00, file); fputc(0x00, file); fputc(0x00, file); // Placeholder for track length
+
+//     // Write tempo meta-event (e.g., 500,000 μs/qn for 120 BPM)
+//     write_tempo_meta_event(file, 500000);
+
+//     //Write all notes to file
+//     writeNotes(notes, file);
+
+//     // Write end-of-track meta-event
+//     fputc(0x00, file);    // Delta time (immediately after last event)
+//     fputc(0xFF, file);    // Meta-event
+//     fputc(0x2F, file);    // End-of-track event type
+//     fputc(0x00, file);    // Length (0 bytes)
+
+//     // Go back and write the actual track length (subtract 8 bytes for "MTrk" and length placeholder)
+//     long end_position = ftell(file);
+//     long track_length = end_position - 22; // Header (14 bytes) + "MTrk" (4 bytes) + length field (4 bytes)
+//     fseek(file, 18, SEEK_SET); // Move to the length field position
+//     fputc((track_length >> 24) & 0xFF, file);
+//     fputc((track_length >> 16) & 0xFF, file);
+//     fputc((track_length >> 8) & 0xFF, file);
+//     fputc(track_length & 0xFF, file);
+//     fseek(file, end_position, SEEK_SET); // Move back to the end of the file
+
+//     freeList(notes);
+//     fclose(file);
+//     return;
+// }
 
 
 
@@ -348,13 +466,15 @@ char getKey()
             && hist[column][3] == hist[column][4] && hist[column][4] == hist[column][5] && hist[column][5] == hist[column][6]
             && hist[column][6] == hist[column][7]) //History is all same to remove debouncing
             {
-                if(headOfNotes == NULL)
-                {
-                    headOfNotes = createNode(hist[col][0], 1.5f);
-                }
-                else
-                {
-                    pushBack(headOfNotes, hist[col][0], 1.5f);
+                if(hist[col][0] != '\0')
+                {    if(headOfNotes == NULL)
+                    {
+                        headOfNotes = createNode(hist[col][0], 1.5f);
+                    }
+                    else
+                    {
+                        pushBack(headOfNotes, hist[col][0], 1.5f);
+                    }
                 }
                 return hist[col][0];
             }
@@ -364,7 +484,7 @@ char getKey()
 
 void drive_column(int c)
 {
-    GPIOC->BSRR = 0xf00000 | ~(1 << (c + 4 + 8));
+    GPIOC->BSRR = 0xf00000 | ~(1 << (c + 4 + 4));
 }
 
 int read_rows()
@@ -416,8 +536,6 @@ int setOctave(char key)
             return 6;
         case '8': 
             return 7;
-        case '9': 
-            return 8;
         
         default:
             return 4;
@@ -708,32 +826,32 @@ int main() {
     // }
     set_freq(0,0);
 
+    freeList(headOfNotes);
+
     for(;;) {
         char key = getKey();
+        //Change note
         if(key == 'A' || key == 'B' || key == 'C' || key == 'D' || key == '*' || key == '0' || key == '#')
         {
-            //Save by pressing "#" and "*"
-            if(key == '#')
-            {
-                col += 2;
-                char key2 = getKey();
-                if(key2 == '*')
-                {
-                    write2File(headOfNotes);
-                    headOfNotes = NULL;
-                    return 0;
-                }
-                col -= 2;
-
-            }
             
             set_freq(0,getNote(key));
         }
-        else if((key == '1' || key == '2' || key == '3' || key == '4' || key == '5' || key == '6' || key == '7' || key == '8' || key == '9'))
+        //Change octave
+        else if((key == '1' || key == '2' || key == '3' || key == '4' || key == '5' || key == '6' || key == '7' || key == '8'))
         {
             OCTAVE = setOctave(key);
         }
+        //Save to buffer
+        else if(key == '9')
+        {
+            write2Array(headOfNotes);
+            set_freq(0,0);
+            //headOfNotes = NULL;
+            return 0;
+        }
     }
+
+    freeList(headOfNotes);
 
     
     
